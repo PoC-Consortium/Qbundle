9b5fa784c9f7d66d71c5c54bf74f7277f8e9a796
Quibus-burst <31744573+Quibus-burst@users.noreply.github.com>
2018-01-08 20:04:27 +0100

    Restarter should no longer cause issues.

--- Restarter/Form1.Designer.vb
+++ Restarter/Form1.Designer.vb
@@ -22,17 +22,27 @@ Partial Class frmRestart
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmRestart))
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(25, 22)
+        Me.Label1.Location = New System.Drawing.Point(36, 16)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(192, 13)
+        Me.Label1.Size = New System.Drawing.Size(161, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Restarting wallet launcher. Please wait."
+        Me.Label1.Text = "Restarting Qbundle. Please wait."
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(25, 32)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(185, 14)
+        Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee
+        Me.ProgressBar1.TabIndex = 1
         '
         'frmRestart
         '
@@ -40,11 +50,13 @@ Partial Class frmRestart
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(241, 58)
+        Me.Controls.Add(Me.ProgressBar1)
         Me.Controls.Add(Me.Label1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "frmRestart"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Restarting Wallet "
+        Me.Text = "Restarting Qbundle"
         Me.TopMost = True
         Me.ResumeLayout(False)
         Me.PerformLayout()
@@ -52,4 +64,5 @@ Partial Class frmRestart
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
 End Class
--- Restarter/Form1.vb
+++ Restarter/Form1.vb
@@ -1,13 +1,19 @@
 ﻿Imports System.IO
+Imports System.Threading
 
 Public Class frmRestart
+    Private Arg As String()
     Private Sub frmRestart_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim Arg As String() = Environment.GetCommandLineArgs()
+        Arg = Environment.GetCommandLineArgs()
+
         If UBound(Arg) < 2 Then
+            MsgBox("Argument Missing")
             End
         End If
+
         Me.Show()
         Me.Label1.Refresh()
+
         Dim Workdir As String = Application.StartupPath
         If Not Workdir.EndsWith("\") Then Workdir &= "\"
         'arg1 Fromfile
@@ -15,35 +21,64 @@ Public Class frmRestart
 
         Try
             If Not IO.File.Exists(Workdir & Arg(1)) Then
+                MsgBox("Argument Missing")
                 End
             End If
 
             If Not IO.File.Exists(Workdir & Arg(2)) Then
+                MsgBox("Argument Missing")
                 End
             End If
-
         Catch ex As Exception
 
         End Try
+        Try
+            Dim trda As Thread
+            trda = New Thread(AddressOf RestartThread)
+            trda.IsBackground = True
+            trda.Start()
+        Catch ex As Exception
+            MsgBox("Failed to Start restarting thread.")
+            End
+        End Try
 
 
+
+
+    End Sub
+
+    Private Sub RestartThread()
+        Dim Workdir As String = Application.StartupPath
+        If Not Workdir.EndsWith("\") Then Workdir &= "\"
+        Dim i As Integer = 0
+        Dim p() As Process
+        Dim PName = Arg(2).Replace(".exe", "")
         Do
-
             Try
                 File.Delete(Workdir & Arg(2)) 'if we can delete file then we can continue
                 File.Copy(Workdir & Arg(1), Workdir & Arg(2), True)
                 Shell(Workdir & Arg(2), AppWinStyle.NormalFocus)
-                Threading.Thread.Sleep(100)
+                Thread.Sleep(100)
                 File.Delete(Workdir & Arg(1))
                 Exit Do
             Catch ex As Exception
 
             End Try
-            Threading.Thread.Sleep(500)
+            Thread.Sleep(500)
+            i += 1
+            If i > 4 Then
+                Try
+                    p = Process.GetProcessesByName(PName)
+                    If p.Count > 0 Then
+                        p(0).Kill()
+                    End If
+                Catch ex As Exception
+
+                End Try
+            End If
         Loop
-
-
         End
+
 
     End Sub
 
